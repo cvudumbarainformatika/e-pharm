82a3aa443a2405bc84ee618b9b1142a6c4598f3c
duro06 <andi.meka.025@gmail.com>
2022-08-15 05:48:43 +0700

    step 1 master layout setting

---
+++ src/layouts/Master/MasterLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout
+    view="lHh LpR lFr"
+    :class="dark?'':'page-light'"
+  >
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-page-sticky
+      position="bottom-right"
+      :offset="[18, 18]"
+    >
+      <q-fab
+        size="sm"
+        padding="sm"
+        icon="icon-mat-display_settings"
+        color="primary"
+        direction="up"
+      >
+        <q-fab-action
+          :color="dark?'warning':'dark'"
+          icon="icon-mat-tungsten"
+          @click="setDark(dark)"
+        />
+      </q-fab>
+    </q-page-sticky>
+  </q-layout>
+</template>
+<script setup>
+
+import { useQuasar } from 'quasar'
+import { computed } from 'vue'
+
+const $q = useQuasar()
+
+const dark = computed(() => {
+  return $q.dark.isActive
+})
+function setDark(val) {
+  const x = !val
+  $q.dark.set(x)
+}
+</script>
