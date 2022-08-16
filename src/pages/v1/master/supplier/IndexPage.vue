22ad4bf994cad1b3826e293c376f1c92d7b3a7a3
duro06 <andi.meka.025@gmail.com>
2022-08-16 22:35:23 +0700

    supplier crud ok

---
+++ src/pages/v1/master/supplier/IndexPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-page class="q-pa-sm">
+    <div>
+      <app-card :is-header="false">
+        <template #content>
+          <app-table
+            title="Data Supplier"
+            :columns="table.columns"
+            :column-hide="table.columnHide"
+            :items="table.items"
+            :meta="table.meta"
+            :per-page="table.params.per_page"
+            :order-by="table.params.order_by"
+            :sort="table.params.sort"
+            :loading="table.loading"
+            :to-search="table.params.q"
+            @goto="table.setPage"
+            @set-row="table.setPerPage"
+            @refresh="table.refreshTable"
+            @find="table.setSearch"
+            @set-order="table.setOder"
+            @new-data="store.newData"
+            @edit-data="store.editData"
+            @delete="table.deletesData"
+          />
+          <!--
+            row-image="image"
+            @delete-ids="table.deletesData"
+            -->
+        </template>
+      </app-card>
+    </div>
+    <!-- dialog -->
+    <formDialog v-model="store.isOpen" />
+  </q-page>
+</template>
+<script setup>
+import { useSupplierTable } from 'src/stores/supplier/table'
+import { useSupplierFormStore } from 'src/stores/supplier/form'
+import formDialog from './FormDialog.vue'
+
+const table = useSupplierTable()
+const store = useSupplierFormStore()
+
+table.getDataTable()
+</script>
