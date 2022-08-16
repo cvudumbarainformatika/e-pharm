acee422cfa565409db3f4b49d71ec1a7030565da
duro06 <andi.meka.025@gmail.com>
2022-08-16 21:20:10 +0700

    Dialognya etinggalan

---
+++ src/pages/v1/master/rak/FormDialog.vue
@@ -0,0 +1,61 @@
+<template>
+  <q-dialog>
+    <app-card
+      class="my-card"
+      title="Form Kategori"
+      desc="Input data Kategori"
+    >
+      <template #content>
+        <q-form
+          ref="formReff"
+          @submit="onSubmit"
+          @reset="onReset"
+        >
+          <div class="row q-col-gutter-md">
+            <div class="col-md-6 col-xs-12">
+              <app-input
+                v-model="store.form.nama"
+                label="Nama*"
+                outlined
+                autofocus
+              />
+            </div>
+          </div>
+          <q-separator class="q-my-md" />
+          <div class="text-right">
+            <app-btn
+              type="reset"
+              color="dark"
+              label="Cancel"
+              class="q-mr-md"
+            />
+            <app-btn
+              label="Simpan"
+              :loading="store.loading"
+            />
+          </div>
+        </q-form>
+      </template>
+    </app-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { useRakFormStore } from 'src/stores/rak/form'
+import { ref } from 'vue'
+
+const store = useRakFormStore()
+
+const formReff = ref(null)
+const onSubmit = () => {
+  store.saveForm().then(() => {
+    console.log('form', formReff)
+    formReff.value.resetValidation()
+  })
+}
+
+const onReset = () => {
+  formReff.value.resetValidation()
+  store.setOpen()
+}
+</script>
