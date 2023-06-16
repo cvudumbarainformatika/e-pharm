f4a5e6cb49f2aa07245f1fc5430b2296b9a5807e
duro06 <andi.meka.025@gmail.com>
2023-06-16 23:38:30 +0700

    location reload

--- src/pages/auth/AuthPage.vue
+++ src/pages/auth/AuthPage.vue
@@ -1,3 +1,5 @@
+<!-- @format -->
+
 <template>
   <q-page class="flex column flex-center">
     <q-card
@@ -8,7 +10,7 @@
       <q-card-section>
         <div
           class="text-center"
-          style="margin-top:30px"
+          style="margin-top: 30px"
         >
           <q-avatar>
             <img src="~assets/logos/logo.png">
@@ -23,7 +25,7 @@
         <q-form
           ref="myForm"
           class="q-pa-md"
-          style="margin-top:70px; z-index: 1;"
+          style="margin-top: 70px; z-index: 1"
           @submit="onSubmit"
         >
           <app-input
@@ -42,7 +44,7 @@
             class="q-mt-sm"
           />
 
-          <div style="margin-top:50px; ">
+          <div style="margin-top: 50px">
             <app-btn
               type="submit"
               :loading="storeAuth.loading"
@@ -54,12 +56,12 @@
       </q-card-section>
       <div
         class="absolute-bottom q-pa-lg text-grey-4"
-        style="z-index: 1;"
+        style="z-index: 1"
       >
         <q-separator />
         <div class="">
-          Aplikasi E-ACHY ini
-          tidak di perjualbelikan secara bebas tanpa seizin dari pembuatnya.
+          Aplikasi E-ACHY ini tidak di perjualbelikan secara bebas tanpa seizin dari
+          pembuatnya.
         </div>
         <div class="q-mt-lg text-right f-14">
           E-ACHY Versi BETA
@@ -83,63 +85,64 @@ const form = ref({
   device_name: $q.platform.is.name + '-' + $q.platform.is.platform
 })
 
-function onSubmit () {
-  storeAuth.login(form.value)
+function onSubmit() {
+  storeAuth.login(form.value).then(() => {
+    window.location.reload()
+  })
 }
-
 </script>
 
 <style lang="scss" scoped>
 .card-login {
-  width:360px;
-  min-height:600px;
+  width: 360px;
+  min-height: 600px;
   overflow: hidden;
   position: relative;
-    &::before {
-      content: "";
-      width:360px;
-      height:200px;
-      position: absolute;
-      border-top-left-radius: 384px;
-        border-top-right-radius: 0px;
-        border-bottom-right-radius: 168%;
-        border-bottom-left-radius: 4%;
-      border-top-style: groove;
-        border-bottom: ridge;
-      background: linear-gradient(-45deg, $secondary, $primary);
-      overflow: hidden;
-    }
+  &::before {
+    content: '';
+    width: 360px;
+    height: 200px;
+    position: absolute;
+    border-top-left-radius: 384px;
+    border-top-right-radius: 0px;
+    border-bottom-right-radius: 168%;
+    border-bottom-left-radius: 4%;
+    border-top-style: groove;
+    border-bottom: ridge;
+    background: linear-gradient(-45deg, $secondary, $primary);
+    overflow: hidden;
+  }
 
-    &::after {
-      content: "";
-      width: 784px;
-        height: 355px;
-        position: absolute;
-        top: 288px;
-        left: -70px;
-        border-width: thick;
-        border-top-width: thick;
-        border-right-width: thick;
-        border-bottom-width: thick;
-        border-left-width: thick;
-        border-top-style: groove;
-        border-top-left-radius: 723%;
-        border-top-right-radius: 0%;
-        border-bottom-right-radius: 0%;
-        border-bottom-left-radius: 12%;
-      background: linear-gradient(-45deg, $secondary, $primary);
-      overflow: hidden;
-    }
+  &::after {
+    content: '';
+    width: 784px;
+    height: 355px;
+    position: absolute;
+    top: 288px;
+    left: -70px;
+    border-width: thick;
+    border-top-width: thick;
+    border-right-width: thick;
+    border-bottom-width: thick;
+    border-left-width: thick;
+    border-top-style: groove;
+    border-top-left-radius: 723%;
+    border-top-right-radius: 0%;
+    border-bottom-right-radius: 0%;
+    border-bottom-left-radius: 12%;
+    background: linear-gradient(-45deg, $secondary, $primary);
+    overflow: hidden;
+  }
 }
 
-@media(max-width:800px){
-    .card-login {
-        width: 100vw;
-        height:100vh;
-        border-radius: 0px;
-          &::before {
-            width: 100vw;
-          }
+@media (max-width: 800px) {
+  .card-login {
+    width: 100vw;
+    height: 100vh;
+    border-radius: 0px;
+    &::before {
+      width: 100vw;
     }
+  }
 }
 </style>
